6d11f2891919b098ab2a32c27a289a1d111dee97
Jukka Puranen <jukka.puranen@gmail.com>
2021-02-28 10:51:25 +0200

    List of companies loaded and displayed

--- trade_permission/pages/index.tsx
+++ trade_permission/pages/index.tsx
@@ -1,7 +1,24 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 
-const IndexPage = () => (
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch('http://localhost:3000/companies.json')
+  const data = await  res.json()
+
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {companies: data}, // will be passed to the page component as props
+  }
+
+}
+
+const IndexPage = (props) => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
@@ -9,6 +26,10 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
+
+  <ul>
+   { props.companies.map( (company) => <li>{company.name} {company.ticker}</li>)}
+  </ul>
   </Layout>
 )
 
