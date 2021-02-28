bf76bd89bae8c43e8b8b82653d11ed8cc2fc5e3f
Jukka Puranen <jukka.puranen@gmail.com>
2021-02-28 11:10:30 +0200

    basic downshift working

--- trade_permission/pages/index.tsx
+++ trade_permission/pages/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Downshift from 'downshift'
 import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -19,6 +20,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 const IndexPage = (props) => (
+
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
@@ -26,10 +28,60 @@ const IndexPage = (props) => (
         <a>About</a>
       </Link>
     </p>
-
+    <Downshift
+    onChange={selection =>
+      alert(selection ? `You selected ${selection.name}` : 'Selection Cleared')
+    }
+    itemToString={item => (item ? item.name : '')}
+  >
+    {({
+      getInputProps,
+      getItemProps,
+      getLabelProps,
+      getMenuProps,
+      isOpen,
+      inputValue,
+      highlightedIndex,
+      selectedItem,
+      getRootProps,
+    }) => (
+      <div>
+        <label {...getLabelProps()}>Enter a company</label>
+        <div
+          style={{display: 'inline-block'}}
+          {...getRootProps({}, {suppressRefError: true})}
+        >
+          <input {...getInputProps()} />
+        </div>
+        <ul {...getMenuProps()}>
+          {isOpen
+            ? 
+                props.companies.filter(item => !inputValue || item.name.includes(inputValue))
+                .map((item, index) => (
+                  <li
+                    {...getItemProps({
+                      key: item.name,
+                      index,
+                      item,
+                      style: {
+                        backgroundColor:
+                          highlightedIndex === index ? 'lightgray' : 'white',
+                        fontWeight: selectedItem === item ? 'bold' : 'normal',
+                      },
+                    })}
+                  >
+                    {item.name}
+                  </li>
+                ))
+            : null}
+        </ul>
+      </div>
+    )}
+  </Downshift>
+{/* 
   <ul>
    { props.companies.map( (company) => <li>{company.name} {company.ticker}</li>)}
-  </ul>
+  </ul> */}
   </Layout>
 )
 
