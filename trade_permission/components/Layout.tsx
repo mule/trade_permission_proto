f7ef2d4cdd657539ea79f6ef15e684d6fee377e6
Jukka Puranen <jukka.puranen@gmail.com>
2021-03-02 12:55:11 +0200

    Bulma added

--- trade_permission/components/Layout.tsx
+++ trade_permission/components/Layout.tsx
@@ -2,6 +2,7 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import Bulma from 'bulma'
 
 type Props = {
   children?: ReactNode
@@ -9,25 +10,29 @@ type Props = {
 }
 
 const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+  <section id="section">
+    <div id="container">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+
+        
+        <nav className="navbar" role="navigation">
+          <Link href="/">
+            <a className="navbar-item">Home</a>
+          </Link> 
+        </nav>  
+      </header>
+      {children}
+      <footer>
+        <hr />
+        <span>I'm here to stay (Footer)</span>
+      </footer>
+    </div>
+  </section>
 )
 
 export default Layout
--- trade_permission/pages/dashboard.tsx
+++ trade_permission/pages/dashboard.tsx
@@ -1,90 +1,86 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import Downshift from 'downshift'
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+import {GetStaticProps, GetStaticPaths, GetServerSideProps} from 'next'
 import useAuth from "../hooks/useAuth";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps : GetServerSideProps = async(context) => {
   const res = await fetch('http://localhost:3000/companies.json')
-  const data = await  res.json()
+  const data = await res.json()
 
   if (!data) {
-    return {
-      notFound: true,
-    }
+    return {notFound: true}
   }
 
   return {
-    props: {companies: data}, // will be passed to the page component as props
+    props: {
+      companies: data
+    }, // will be passed to the page component as props
   }
 
 }
 
+export default function DashboardPage(props) {
 
+  const {user, loading} = useAuth();
 
-export default function DashboardPage (props)  { 
-
-  const { user, loading } = useAuth();
-  
-  
   return (
 
-   
-  <Layout title="Home | Next.js + TypeScript Example">
-    <Downshift
-    onChange={selection =>
-      alert(selection ? `You selected ${selection.name}` : 'Selection Cleared')
-    }
-    itemToString={item => (item ? item.name : '')}
-  >
-    {({
-      getInputProps,
-      getItemProps,
-      getLabelProps,
-      getMenuProps,
-      isOpen,
-      inputValue,
-      highlightedIndex,
-      selectedItem,
-      getRootProps,
-    }) => (
-      <div>
-        <label {...getLabelProps()}>Enter a company</label>
-        <div
-          style={{display: 'inline-block'}}
-          {...getRootProps({}, {suppressRefError: true})}
-        >
-          <input {...getInputProps()} />
+    <Layout title="Home | Next.js + TypeScript Example">
+      
+      <div className="columns mx-1">
+        <div className="column">
+        <h1 className="title">Lupa</h1>
+          <Downshift
+            onChange={selection => alert(selection
+            ? `You selected ${selection.name}`
+            : 'Selection Cleared')}
+            itemToString={item => (item
+            ? item.name
+            : '')}>
+            {({
+              getInputProps,
+              getItemProps,
+              getLabelProps,
+              getMenuProps,
+              isOpen,
+              inputValue,
+              highlightedIndex,
+              selectedItem,
+              getRootProps
+            }) => (
+              <div>
+                <label className="label" {...getLabelProps()}>Yritys</label>
+                <div
+                  style={{
+                  display: 'inline-block'
+                }}
+                  {...getRootProps({}, {suppressRefError: true})}>
+                  <input className="input" placeholder="Yrityksen nimi" {...getInputProps()}/>
+                </div>
+                <ul {...getMenuProps()}>
+                  {isOpen
+                    ? props
+                      .companies
+                      .filter(item => !inputValue || item.name.includes(inputValue))
+                      .map((item, index) => (
+                        <li
+                          {...getItemProps({ key: item.name, index, item, style: { backgroundColor: highlightedIndex === index ? 'lightgray' : 'white', fontWeight: selectedItem === item ? 'bold' : 'normal', }, })}>
+                          {item.name}
+                        </li>
+                      ))
+                    : null}
+                </ul>
+              </div>
+            )}
+          </Downshift>
         </div>
-        <ul {...getMenuProps()}>
-          {isOpen
-            ? 
-                props.companies.filter(item => !inputValue || item.name.includes(inputValue))
-                .map((item, index) => (
-                  <li
-                    {...getItemProps({
-                      key: item.name,
-                      index,
-                      item,
-                      style: {
-                        backgroundColor:
-                          highlightedIndex === index ? 'lightgray' : 'white',
-                        fontWeight: selectedItem === item ? 'bold' : 'normal',
-                      },
-                    })}
-                  >
-                    {item.name}
-                  </li>
-                ))
-            : null}
-        </ul>
       </div>
-    )}
-  </Downshift>
-{/* 
+      {/*
   <ul>
    { props.companies.map( (company) => <li>{company.name} {company.ticker}</li>)}
   </ul> */}
-  </Layout>)
+    </Layout>
+  )
 
 }
